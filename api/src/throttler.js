1c408154d4abe4dfc31ec98ff3ece3b9784077d9
Laurent Réveillère <laurent.reveillere@u-bordeaux.fr>
2023-10-24 15:55:50 +0200

    limiter rps

--- api/src/throttler.js
+++ api/src/throttler.js
@@ -1,32 +1,50 @@
 import fetch from 'node-fetch';
+import Bottleneck from 'bottleneck';
 
 class QueueThrottler {
   constructor() {
     this.queue = [];
+
+    // Créer des limiters spécifiques pour chaque domaine
+    this.limiters = {
+      'dblp.org': new Bottleneck({ maxConcurrent: 1, minTime: 100 }),
+      'portal.core.edu.au': new Bottleneck({ maxConcurrent: 1, minTime: 100 }),
+    };
+  }
+
+  getLimiterForURL(url) {
+    for (const domain in this.limiters) {
+      if (url.includes(domain)) {
+        return this.limiters[domain];
+      }
+    }
+    // Si aucun domaine correspondant n'est trouvé, retourner une instance sans restriction
+    return new Bottleneck({ maxConcurrent: 1000, minTime: 0 });
   }
 
   async processQueue() {
     if (this.queue.length) {
       const { request, resolve, reject } = this.queue.shift();
+      const limiter = this.getLimiterForURL(request.url);
 
-      try {
-        const response = await request();
+      limiter.schedule(async () => {
+        try {
+          const response = await request();
 
-        if (response.status === 429) {
-          const retryAfter = parseInt(response.headers.get('Retry-After'), 10) * 1000; 
-          console.error(`Too many requests to ${request}, retrying in ${retryAfter}ms...`);
-          setTimeout(() => {
-            this.queue.unshift({ request, resolve, reject }); 
-            this.processQueue();
-          }, retryAfter);
-        } else {
-          resolve(response);
+          if (response.status === 429) {
+            const retryAfter = parseInt(response.headers.get('Retry-After'), 10) * 1000;
+            console.error(`Too many requests to ${request.url}, retrying in ${retryAfter}ms...`);
+            setTimeout(() => {
+              this.queue.unshift({ request, resolve, reject });
+              this.processQueue();
+            }, retryAfter);
+          } else {
+            resolve(response);
+          }
+        } catch (error) {
+          reject(error);
         }
-      } catch (error) {
-        reject(error);
-      }
-
-      this.processQueue();
+      });
     }
   }
 
